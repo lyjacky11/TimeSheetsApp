9d671bdebe1ffd4c4dc7df8afe7b67d2988a4815
lyjacky11 <lyjacky@live.com>
2019-06-04 16:27:54 -0400

    Added "About Developer" section in the Action Bar

--- app/src/main/java/com/lyjacky11/timesheet/MainActivity.java
+++ app/src/main/java/com/lyjacky11/timesheet/MainActivity.java
@@ -1,22 +1,35 @@
 package com.lyjacky11.timesheet;
 
+import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Handler;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.view.ViewTreeObserver;
 import android.webkit.JsResult;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Dialog dialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        dialog = new Dialog(this);
 
         final WebView webView = findViewById(R.id.webView);
         webView.loadUrl("file:///android_asset/index.html");
@@ -65,7 +78,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        final SwipeRefreshLayout swipeRefreshLayout = findViewById(R.id.swipelayout);
+        final SwipeRefreshLayout swipeRefreshLayout = findViewById(R.id.swipeLayout);
         swipeRefreshLayout.setColorSchemeResources(R.color.colorAccent);
         webView.getViewTreeObserver().addOnScrollChangedListener(new ViewTreeObserver.OnScrollChangedListener() {
             @Override
@@ -91,5 +104,66 @@ public class MainActivity extends AppCompatActivity {
                 }, 500);
             }
         });
+    }
+
+    public void goToGithub (View view) {
+        goToUrl("https://github.com/lyjacky11");
+    }
+
+    public void goToLinkedin (View view) {
+        goToUrl("https://www.linkedin.com/in/lyjacky11/");
+    }
+
+    public void goToWeb(View view) {
+        goToUrl("https://timesheet.lyjacky11.me/");
+    }
+
+    public void goToWebsite (View view) {
+        goToUrl("https://lyjacky11.me/");
+    }
+
+    public void goToYoutube (View view) {
+        goToUrl("https://www.youtube.com/user/LyJacky");
+    }
+
+    private void goToUrl (String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_about:
+                ImageView closeBtn;
+                dialog.setContentView(R.layout.popup);
+                closeBtn = dialog.findViewById(R.id.closeBtn);
+                closeBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+                TextView versionName = dialog.findViewById(R.id.version);
+                versionName.setText("App version: " + BuildConfig.VERSION_NAME);
+                dialog.setCanceledOnTouchOutside(false);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
