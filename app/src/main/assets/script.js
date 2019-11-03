1c2dcbc2b59f6d3a9332919aea13f60dfdc564a6
lyjacky11 <lyjacky@live.com>
2019-11-03 13:49:20 -0500

    Changed app design and UI

--- app/src/main/assets/script.js
+++ app/src/main/assets/script.js
@@ -1,14 +1,14 @@
 var counter = 1;
 
 function calculateHours() {
-	let pay_rate = document.getElementById("pay_rate").value;
-	let rate_x = document.getElementById("rate_x").value;
-	let start_shift = document.getElementById("start_shift").value;
-	let end_shift = document.getElementById("end_shift").value;
-	let hours_worked = document.getElementById("hours_worked");
-	let total_breaks = document.getElementById("total_breaks");
-	let gross_pay = document.getElementById("gross_pay");
-	let results = document.getElementById("results");
+	var pay_rate = document.getElementById("pay_rate").value;
+	var rate_x = document.getElementById("rate_x").value;
+	var start_shift = document.getElementById("start_shift").value;
+	var end_shift = document.getElementById("end_shift").value;
+	var hours_worked = document.getElementById("hours_worked");
+	var total_breaks = document.getElementById("total_breaks");
+	var gross_pay = document.getElementById("gross_pay");
+	var results = document.getElementById("results");
 
 	if (start_shift != "" && end_shift != "") {
 
@@ -19,16 +19,16 @@ function calculateHours() {
 		var end_minute = end_shift.split(":")[1];
 
 		// Calculate shift hours
-		var net_start = Number(start_hour) + start_minute/60;
-		var net_end = Number(end_hour) + end_minute/60;
+		var net_start = Number(start_hour) + start_minute / 60;
+		var net_end = Number(end_hour) + end_minute / 60;
 		var total_shift_hours = net_end - net_start;
 		var totalb_hours = 0;
 
 		for (var i = 1; i <= counter; i++) {
-			let start_id = "start_break" + i;
-			let end_id = "end_break" + i;
-			let start_break = document.getElementById(start_id).value;
-			let end_break = document.getElementById(end_id).value;
+			var start_id = "start_break" + i;
+			var end_id = "end_break" + i;
+			var start_break = document.getElementById(start_id).value;
+			var end_break = document.getElementById(end_id).value;
 			if (start_break != "" && end_break != "") {
 				// Break Times
 				var startb_hour = start_break.split(":")[0];
@@ -37,8 +37,8 @@ function calculateHours() {
 				var endb_minute = end_break.split(":")[1];
 
 				// Calculate break hours
-				var netb_start = Number(startb_hour) + startb_minute/60;
-				var netb_end = Number(endb_hour) + endb_minute/60;
+				var netb_start = Number(startb_hour) + startb_minute / 60;
+				var netb_end = Number(endb_hour) + endb_minute / 60;
 				if (netb_end < netb_start || netb_start < net_start || netb_end > net_end) {
 					alert("Invalid break times!\nPlease check the information and try again!");
 					return false;
@@ -53,10 +53,10 @@ function calculateHours() {
 		var total_amount = pay_rate * rate_x * total_hours;
 
 		var hour_netdiff = Math.floor(total_hours);
-		var minute_netdiff = Math.floor((total_hours % 1) * 60);
+		var minute_netdiff = Math.round((total_hours % 1) * 60);
 
 		var hour_bdiff = Math.floor(totalb_hours);
-		var minute_bdiff = Math.floor((totalb_hours % 1) * 60);
+		var minute_bdiff = Math.round((totalb_hours % 1) * 60);
 
 		totalb_hours = totalb_hours.toFixed(2);
 		total_hours = total_hours.toFixed(2);
@@ -74,18 +74,18 @@ function calculateHours() {
 }
 
 function addBreak() {
-	let breaks = document.getElementById("breaks");
-	let currentID = "break" + counter;
-	let break_num = document.getElementById(currentID);
-	let removeButton = document.getElementById("removeButton");
+	var breaks = document.getElementById("breaks");
+	var currentID = "break" + counter;
+	var break_num = document.getElementById(currentID);
+	var removeButton = document.getElementById("removeButton");
 
 	clone = break_num.cloneNode(true);
 	counter++;
-	let newID = "break" + counter;
+	var newID = "break" + counter;
 	clone.id = newID;
 
-	let start_break = clone.getElementsByTagName("input")[0];
-	let end_break = clone.getElementsByTagName("input")[1];
+	var start_break = clone.getElementsByTagName("input")[0];
+	var end_break = clone.getElementsByTagName("input")[1];
 	start_break.id = "start_break" + counter;
 	end_break.id = "end_break" + counter;
 
@@ -94,26 +94,28 @@ function addBreak() {
 }
 
 function removeBreak() {
-	let removeButton = document.getElementById("removeButton");
-	if (counter > 1)
-	{
-		let currentID = "break" + counter;
-		let break_num = document.getElementById(currentID);
+	var removeButton = document.getElementById("removeButton");
+	if (counter > 1) {
+		var currentID = "break" + counter;
+		var break_num = document.getElementById(currentID);
 		break_num.remove();
 		counter--;
-		if (counter == 1) { removeButton.style.visibility = "hidden"; }
+		if (counter == 1) {
+			removeButton.style.visibility = "hidden";
+		}
 	}
 }
 
 function clearForm() {
-	let hours_worked = document.getElementById("hours_worked");
-	let total_breaks = document.getElementById("total_breaks");
-	let gross_pay = document.getElementById("gross_pay");
-	let shift_times = document.getElementById("shift_times");
-	let results = document.getElementById("results");
+	var hours_worked = document.getElementById("hours_worked");
+	var total_breaks = document.getElementById("total_breaks");
+	var gross_pay = document.getElementById("gross_pay");
+	var shift_times = document.getElementById("shift_times");
 
 	shift_times.reset();
-	while (counter != 1) { removeBreak(); }
+	while (counter != 1) {
+		removeBreak();
+	}
 	hours_worked.innerHTML = "Hours Worked: 0h 0m | 0.00 hours";
 	total_breaks.innerHTML = "Total Breaks: 0h 0m | 0.00 hours";
 	gross_pay.innerHTML = "Gross Pay: $0.00";
